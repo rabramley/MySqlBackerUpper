cd86d648164a72920b94e112a88da6b79105227c
Richard Bramley <rabramley@gmail.com>
2016-08-03 09:27:03 +0100

    Initial check in

---
+++ backupAllDatabases.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR=$(dirname "$0")
+
+if [[ $1 == D ]]; then
+    BACKUP_PERIOD="daily"
+    REMOVE_TIMESPAN_DAYS=+6
+elif [[ $1 == W ]]; then
+    BACKUP_PERIOD="weekly"
+    REMOVE_TIMESPAN_DAYS=+21
+elif [[ $1 == M ]]; then
+    BACKUP_PERIOD="monthly"
+    REMOVE_TIMESPAN_DAYS=+93
+else
+    echo "Time period parameter not supplied should be D, W or M"
+    exit 1
+fi
+
+TIMESTAMP=$(date +"%F")
+BACKUP_DIR="$SCRIPTDIR/backup/$BACKUP_PERIOD"
+LATEST_DIR="$SCRIPTDIR/backup/latest"
+OPTIONS_FILE=$SCRIPTDIR/mysql_options.cnf
+
+mkdir -p "$BACKUP_DIR"
+rm -fR "$LATEST_DIR"
+mkdir -p "$LATEST_DIR"
+
+databases=`mysql --defaults-extra-file=$OPTIONS_FILE -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+
+for db in $databases; do
+  mysqldump --defaults-extra-file=$OPTIONS_FILE --force --opt --databases --events --routines --triggers $db | gzip > "$BACKUP_DIR/$db-$TIMESTAMP.gz"
+
+  ln -s "$BACKUP_DIR/$db-$TIMESTAMP.gz" "$LATEST_DIR/$db-$TIMESTAMP.gz"
+done
+
+find $BACKUP_DIR -ctime $REMOVE_TIMESPAN_DAYS -exec rm {} +
